ce1dc1fef0696b3dfc49f493378023f1b06df0c8
johan <johan.schantz@telia.com>
2017-02-08 14:53:10 +0100

    add set expire date

--- lib/account.rb
+++ lib/account.rb
@@ -1,10 +1,14 @@
 class Account
-  attr_accessor :pin_code
-
+  attr_accessor :pin_code, :exp_date
+  STANDARD_VALIDITY_YRS = 5
   def initialize
     @pin_code = generate_pin
+    @exp_date = set_expire_date
   end
 
+  def set_expire_date
+    Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
+end
 
   private
 
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -7,4 +7,8 @@ describe Account do
    number_length = Math.log10(subject.pin_code).to_i + 1
    expect(number_length).to eq 4
   end
+ it 'is expect an expiry date on initialize' do
+   expected_date = Date.today.next_year(5).strftime("%m/%y")
+   expect(subject.set_expire_date).to eq expected_date
+  end
 end
