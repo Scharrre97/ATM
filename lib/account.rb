dbc8d4ca6697df78eca108aa7bfa75f385cd3d60
johan <johan.schantz@telia.com>
2017-02-09 10:37:24 +0100

    Person class - added to have a name on initialize

--- lib/account.rb
+++ lib/account.rb
@@ -1,6 +1,13 @@
 class Account
-  attr_accessor :pin_code, :exp_date, :balance, :account_status, :owner
+
+  attr_accessor :pin_code,
+                :exp_date,
+                :balance,
+                :account_status,
+                :owner
+
   STANDARD_VALIDITY_YRS = 5
+
   def initialize(attrs = {})
     @pin_code = generate_pin
     @exp_date = set_expire_date
@@ -15,7 +22,7 @@ class Account
 
   def set_expire_date
     Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
-end
+  end
 
   private
 
---
+++ spec/person_spec.rb
@@ -0,0 +1,20 @@
+require './lib/person'
+require './lib/atm'
+require './lib/account'
+
+describe Person do
+
+  subject { described_class.new(name: 'Johan') }
+
+ it 'is expected to have a :name on initialize' do
+   expect(subject.name).not_to be nil
+ end
+
+ it 'is expected to have name Johan' do
+   expect(subject.name).to eq 'Johan'
+ end
+
+ it 'is expected to raise error if no name is set' do
+   expect { described_class.new }.to raise_error 'A name is required'
+ end
+end
